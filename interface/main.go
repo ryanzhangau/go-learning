faa922115ba47dbaf72481533a26a95d3871cad7
ryanzhangau <ryanzhangau@gmail.com>
2022-03-28 20:09:50 +1100

    interface

---
+++ interface/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberofTeeth int
+}
+
+func main() {
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "German Shephered",
+	}
+
+	PrintInfo(&dog)
+
+	gorilla := Gorilla{
+		Name:          "Gorilla",
+		Color:         "grey",
+		NumberofTeeth: 8,
+	}
+
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (d *Gorilla) NumberOfLegs() int {
+	return 2
+}
